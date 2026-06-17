541811d5e0df5fd8eb34bb100faac6f0c33dcb07
chenzhuo.li <lichenzhuo666@gmail.com>
2026-06-17 22:04:50 +0800

    优化调试

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,31 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
   plugins: [
+    vueDevTools({
+      launchEditor: 'cursor',
+      componentInspector: {
+        // devtools 默认会关掉快捷键和按钮，这里显式打开
+        toggleComboKey: 'control-shift',
+        toggleButtonVisibility: 'active',
+        launchEditor: 'cursor',
+      },
+    }),
     vue(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
       imports: ['vue', 'vue-router', 'pinia'],
+      dts: false,
     }),
     Components({
       resolvers: [ElementPlusResolver()],
+      dts: false,
     }),
   ],
   resolve: {
